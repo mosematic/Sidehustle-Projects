3354800f127d901891b2d51fe22dd25ef5cad25d
Mubangizi Moses <moselee10@gmail.com>
2022-09-16 20:26:25 +0300

    Update odin.js

--- odin.js
+++ odin.js
@@ -1,11 +1,11 @@
-//first grabbed the id of the button and added an event listener and function name
+//added an event listener and function name
 document.querySelector('#btn').addEventListener('click', validate)
 //created the function
 function validate() {
   //assigned the password and confirmPassword inputs to various variables
   let password = document.querySelector("#password");
   let confirmPassword = document.querySelector("#confirmPassword");
-  //added a thin solid red border(like an error class) if the input values are empty
+  //added a thin solid red border(like an error class) 
   if (password.value === "") {
     password.style.border = "thin solid red";
   } else {
@@ -16,7 +16,7 @@ function validate() {
   } else {
     confirmPassword.style.border = "1px solid rgba(199, 196, 196, 0.521)";
   }
-  //added a thin solid red border and some text "Passwords do not match" incase the passwords do not match
+  //added a thin solid red border and some text "Passwords do not match" 
   if (password.value !== confirmPassword.value) {
     password.style.border = "thin solid red";
     confirmPassword.style.border = "thin solid red";
